21e963f7a6ea2820a212a696b3db8cd290144b15
NikeshDindy <nikeshdindy222@gmail.com>
2025-09-09 12:20:33 +0530

    UnitOfWork interface Implementation

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using InventoryManagement.Data;
+using InventoryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IRepository<Product> Products { get; }
+        public IRepository<Category> Categories { get; }
+        public IRepository<Supplier> Suppliers { get; }
+        public IRepository<Order> Orders { get; }
+        public IRepository<OrderDetail> OrderDetails { get; }
+        public IRepository<InventoryTransaction> Transactions { get; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Products = new Repository<Product>(_context);
+            Categories = new Repository<Category>(_context);
+            Suppliers = new Repository<Supplier>(_context);
+            Orders = new Repository<Order>(_context);
+            OrderDetails = new Repository<OrderDetail>(_context);
+            Transactions = new Repository<InventoryTransaction>(_context);
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}

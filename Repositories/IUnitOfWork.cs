b87c0bb2e54ba73428879e7309812931c7c0db4a
NikeshDindy <nikeshdindy222@gmail.com>
2025-09-09 12:13:26 +0530

    all the unit IRepos

---
+++ Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagement.Models;
+
+namespace InventoryManagement.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Product> Products { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<Supplier> Suppliers { get; }
+        IRepository<Order> Orders { get; }
+        IRepository<OrderDetail> OrderDetails { get; }
+        IRepository<InventoryTransaction> Transactions { get; }
+
+        Task<int> CompleteAsync();
+    }
+}

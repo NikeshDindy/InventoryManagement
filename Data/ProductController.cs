a2ca5c9159a8bf949183d1b8e8966963592ba949
Aditi <aditiraj@ibm.com>
2025-09-09 11:58:03 +0530

    productcontroller

---
+++ Data/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Data
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

b30cc6d8d48fb0448b046fa702dd2150bc2cdb5f
Navya1010777 <navya10lily@gmail.com>
2025-09-11 10:21:14 +0530

    Transaction Index view

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using InventoryManagement.Models;
 using InventoryManagement.Repositories;
 using InventoryManagement.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
